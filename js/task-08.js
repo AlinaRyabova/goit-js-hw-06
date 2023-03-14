66f9ba8a8030f75f3c0e453c2ec22c6b895c3a38
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2023-03-14 19:35:36 +0200

    add task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const form = document.querySelector(`.login-form`);
+form.addEventListener(`submit`, onSubmit);
+
+function onSubmit(event){
+    event.preventDefault();
+
+
+const email = form.elements.email.value;
+const password = form.elements.password.value;
+
+if ( !email || !password){
+    alert(`All fields must be filled!`);
+    return;
+}
+
+const formData = {
+    email, 
+    password,
+};
+
+console.log(formData);
+
+form.reset ();
+
+};
 
